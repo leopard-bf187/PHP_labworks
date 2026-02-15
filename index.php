5855c5fa0444cd66686d4e9882505d8296dc5f82
leopard_bf187 <leonidparmacli@gmail.com>
2026-02-15 19:51:53 +0200

    lab2 ready

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "hello world\n";
+echo "Hello", "World with echo! <br>\n" . PHP_EOL;
+print "Hello, World with print! <br>\n" . PHP_EOL;
+
+$days = 288;
+$message = "Все возвращаемся на работу";
+
+echo "Количество дней: " . $days . "<br />";
+echo $message . "<br />";
+
+?>
